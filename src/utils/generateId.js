9db1648678161a1437f8b759e74201e732a6b2d1
SergeyScherbo <sergey31scherbo@gmail.com>
2018-10-15 21:54:25 +0400

    Revert "Develop create"

--- src/utils/generateId.js
+++ src/utils/generateId.js
@@ -1,6 +1,7 @@
 const generateId = (idLength) => {
 	const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 	const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+	const symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+'];
 
 	let id = '';
 
@@ -9,8 +10,10 @@ const generateId = (idLength) => {
 
 		if (nextSymbol === 0) {
 			id += letters[Math.floor(Math.random() * letters.length)];
-		} else {
+		} else if (nextSymbol === 1) {
 			id += numbers[Math.floor(Math.random() * numbers.length)];
+		} else {
+			id += symbols[Math.floor(Math.random() * symbols.length)];
 		}
 	}
 
