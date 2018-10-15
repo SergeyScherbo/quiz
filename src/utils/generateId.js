72353f1d2f5622fde4b1e5b7a01c30c30d0f2591
Scherbo <sergey31scherbo@gmail.com>
2018-10-15 21:33:26 +0400

    simplify generateId function, to make valid query strings

--- src/utils/generateId.js
+++ src/utils/generateId.js
@@ -1,7 +1,6 @@
 const generateId = (idLength) => {
 	const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 	const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-	const symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+'];
 
 	let id = '';
 
@@ -10,10 +9,8 @@ const generateId = (idLength) => {
 
 		if (nextSymbol === 0) {
 			id += letters[Math.floor(Math.random() * letters.length)];
-		} else if (nextSymbol === 1) {
-			id += numbers[Math.floor(Math.random() * numbers.length)];
 		} else {
-			id += symbols[Math.floor(Math.random() * symbols.length)];
+			id += numbers[Math.floor(Math.random() * numbers.length)];
 		}
 	}
 
