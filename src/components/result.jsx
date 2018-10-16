909b22f829fdefda9649bb8b38d45c3ef2251ee1
Scherbo <sergey31scherbo@gmail.com>
2018-10-16 16:50:30 +0400

    result component

---
+++ src/components/result.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Result = ({ progress }) => {
+	const correctAnswers = progress.filter(p => p === 1);
+	const percentage = correctAnswers.length / progress.length * 100;
+	let message = '';
+
+	if (percentage <= 50) {
+		message = `Sorry, but only ${percentage}% of your answers are correct... Next time will be better!`;
+	} else if (percentage > 50 && percentage < 90) {
+		message = `Very good, ${percentage}% of your answers are correct!`;
+	} else {
+		message = `Awesome, you have result with ${percentage}% correct answers!`;
+	}
+
+	return (
+		<React.Fragment>
+			<h3 className="heading heading--l3">{message}</h3>
+			<div className="btn-group btn-group--mtop">
+				<Link className="btn btn--primary" to="/">Return</Link>
+			</div>
+		</React.Fragment>
+	);
+}
+
+export default Result;
