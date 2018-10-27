16b647cee8f16a1877799ee8ec24cd82aa25c4f5
Scherbo <sergey31scherbo@gmail.com>
2018-10-27 23:01:55 +0400

    no permission component

---
+++ src/components/routes/noPermission.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NoPermission = () => {
+	return (
+		<div className="pageStart">
+			<p className="text">Sorry, but you don't have permission to access this page...</p>
+		</div>
+	);
+}
+
+export default NoPermission;
