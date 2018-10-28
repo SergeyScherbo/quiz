2b2f47fcb24f772f8dbe95e78150865fa7f808d2
Scherbo <sergey31scherbo@gmail.com>
2018-10-28 23:27:11 +0400

    login, based on form component

--- src/components/routes/login.jsx
+++ src/components/routes/login.jsx
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import Form from '../common/form';
 
-class Login extends Component {
-	state = {};
+class Login extends Form {
+	state = {
+		email: '',
+		password: ''
+	};
+
+	handleSubmit = (event) => {
+		event.preventDefault();
+
+		console.log(this.state);
+		// server request...
+	}
 
 	render() {
 		return (
-			<h1>Login page</h1>
+			<div className="form">
+				<p className="text">Login form</p>
+				<form onSubmit={this.handleSubmit}>
+					{this.renderInput('email', 'Email', 'email')}
+					{this.renderInput('password', 'Password', 'password')}
+					{this.renderButton('Login')}
+
+					<p className="text">Forgot your password?</p>
+					<p className="text">Do not have an account?</p>
+				</form>
+			</div>
 		);
 	}
 }
